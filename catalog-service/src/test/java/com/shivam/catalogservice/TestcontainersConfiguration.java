f6f5d5a88ae07053a9cb1e4a07747a7f342c982e
Shivam Nagapure <shivamnagapure2005@gmail.com>
2026-02-27 15:42:27 +0530

    creating products order table and inserting dummy data using flyway

--- catalog-service/src/test/java/com/shivam/catalogservice/TestcontainersConfiguration.java
+++ catalog-service/src/test/java/com/shivam/catalogservice/TestcontainersConfiguration.java
@@ -7,7 +7,7 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+class  TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
