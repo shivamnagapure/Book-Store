89dc22da1b71bd16e4d5dcacbb2cbeaff98c097c
Shivam Nagapure <shivamnagapure2005@gmail.com>
2026-03-20 21:51:20 +0530

    RabbitMQ configuration

--- order-service/src/main/java/com/shiv/bookstore/orders/OrderServiceApplication.java
+++ order-service/src/main/java/com/shiv/bookstore/orders/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.shiv.bookstore.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
---
+++ order-service/src/main/java/com/shiv/bookstore/orders/config/RabbitMQConfig.java
@@ -0,0 +1,65 @@
+package com.shiv.bookstore.orders.config;
+
+import com.shiv.bookstore.orders.ApplicationProperties;
+import org.springframework.amqp.core.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    private final ApplicationProperties properties ;
+
+    RabbitMQConfig(ApplicationProperties properties){
+        this.properties = properties ;
+    }
+
+    @Bean
+    DirectExchange exchange(){
+        return new DirectExchange(properties.orderEventsExchange());
+    }
+
+    @Bean
+    Queue newOrdersQueue(){
+        return QueueBuilder.durable(properties.newOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding newOrdersQueueBinding(){
+        return BindingBuilder.bind(newOrdersQueue())
+                .to(exchange())
+                .with(properties.newOrdersQueue()); // routing key (can be anything)
+    }
+
+    @Bean
+    Queue deliveredOrdersQueue() {
+        return QueueBuilder.durable(properties.deliveredOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding deliveredOrdersQueueBinding() {
+        return BindingBuilder.bind(deliveredOrdersQueue()).to(exchange()).with(properties.deliveredOrdersQueue());
+    }
+
+    @Bean
+    Queue cancelledOrdersQueue() {
+        return QueueBuilder.durable(properties.cancelledOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding cancelledOrdersQueueBinding() {
+        return BindingBuilder.bind(cancelledOrdersQueue()).to(exchange()).with(properties.cancelledOrdersQueue());
+    }
+
+    @Bean
+    Queue errorOrdersQueue() {
+        return QueueBuilder.durable(properties.errorOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding errorOrdersQueueBinding() {
+        return BindingBuilder.bind(errorOrdersQueue()).to(exchange()).with(properties.errorOrdersQueue());
+    }
+
+}
