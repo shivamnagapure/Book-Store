8c8920b366126d81fb47ae3c9404c639a3e88082
Shivam Nagapure <shivamnagapure2005@gmail.com>
2026-04-21 19:28:53 +0530

    Cors config for swagger

--- order-service/src/main/java/com/shiv/bookstore/orders/WebMvcConfig.java
+++ order-service/src/main/java/com/shiv/bookstore/orders/WebMvcConfig.java
@@ -6,13 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-// browser requests -> spring responses with these headers -> browser checks Origin Allowed , Method Allowed , Headers Allowed etc .
+    // browser requests -> spring responses with these headers -> browser checks Origin Allowed , Method Allowed ,
+    // Headers Allowed etc .
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**") //allow request starting with /api/
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // allow request starting with /api/
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowedOriginPatterns("*") //Allow requests from ANY domain
-                .allowCredentials(false) ; //Cookies not allowed
+                .allowedOriginPatterns("*") // Allow requests from ANY domain
+                .allowCredentials(false); // Cookies not allowed
     }
 }
