d730f9fded383653852ac5ca0e1099b8bf611306
Shivam Nagapure <shivamnagapure2005@gmail.com>
2026-05-17 14:23:54 +0530

    commit 4

--- order-service/src/main/java/com/shiv/bookstore/orders/OrderServiceApplication.java
+++ order-service/src/main/java/com/shiv/bookstore/orders/OrderServiceApplication.java
@@ -14,8 +14,5 @@ public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
-        /*
-        comment
-         */
     }
 }
